430f60debd6a892201b69765d7c6b002298807d1
32Nandhini <anand21nandhini@gmail.com>
2024-06-29 19:58:18 +0530

    Javascript objects

---
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -0,0 +1,26 @@
+// A blog post
+
+var post = {
+	id : 1,
+	title : "My Crazy Space Adventure",
+	author : "Philae",
+	created : "2015-06-21",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+var post2 = {
+	id : 2,
+	title : "House of Dragon",
+	author : "George .R.R",
+	created : "1998-06-9",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+
+/* Further Adventures
+ *
+ * 1) Display some of the properties.
+ *
+ * 2) Add a second post.
+ *
+ */
