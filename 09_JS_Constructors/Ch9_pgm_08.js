0d1691e69845b415b93b446db05be57695f4a027
32Nandhini <anand21nandhini@gmail.com>
2024-07-14 14:08:16 +0530

    Constructors

--- 09_JS_Constructors/Ch9_pgm_08.js
+++ 09_JS_Constructors/Ch9_pgm_08.js
@@ -30,6 +30,45 @@ var calEvent = new CalendarEvent(
 calEvent.showEvent();
 
 
+// Define the CalendarEvent constructor with the updated showEvent method
+var CalendarEvent = function (title, startDate, startTime, endTime) {
+    this.title = title;
+    this.startDate = startDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  
+    this.showEvent = function () {
+        var dateString = [
+            this.startDate,
+            " - (",
+            this.startTime,
+            " - ",
+            this.endTime,
+            ")"
+        ].join("");
+      
+        console.log(this.title + ": " + dateString);
+    };
+};
+
+// Create the first event
+var calEvent1 = new CalendarEvent(
+    "Annual Review",
+    "3/5/16",
+    "4.00pm",
+    "5.00pm"
+);
+
+calEvent1.showEvent(); 
+
+var calEvent2 = new CalendarEvent(
+    "Team Meeting",
+    "3/6/16",
+    "10.00am",
+    "11.00am"
+);
+
+calEvent2.showEvent(); 
 
 /* Further Adventures
  *
