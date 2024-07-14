a4679e5aa9f8a18250fb18f8cabea709bf4ab0c2
32Nandhini <anand21nandhini@gmail.com>
2024-07-14 14:18:16 +0530

    Promises

--- 11_JS_Promises/async_await.js
+++ 11_JS_Promises/async_await.js
@@ -1,54 +1,26 @@
-// Async and await is an elegant way to handle promises. It is easy to understand and it clean to write.
+//Using Promises:
+const url = 'https://restcountries.com/v2/all';
 
-// const square = async function (n) {
-//     return n * n
-//   }
-  
-//   square(2)
-
-//   Promise {<resolved>: 4}
-
-
-
-// The word async in front of a function means that function will return a promise. 
-// The above square function instead of a value it returns a promise.
-
-// How do we access the value from the promise? 
-// To access the value from the promise, we will use the keyword await.
-
-
-const square = async function (n) {
-    return n * n
-  }
-  const value = await square(2)
-  console.log(value)
-
-// 4
-
-
-// Let us fetch API data using both promise method and async and await method.
-
-// promise
-
-const url = 'https://restcountries.com/v2/all'
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    console.log(data);
   })
-  .catch(error => console.error(error))
+  .catch(error => {
+    console.error(error);
+  });
 
-
-//  async and await
-
-const fetchData = async () => {
+  //Using Async/Await:
+  const fetchData = async () => {
     try {
-      const response = await fetch(url)
-      const countries = await response.json()
-      console.log(countries)
-    } catch (err) {
-      console.error(err)
+      const response = await fetch(url);
+      const countries = await response.json();
+      console.log(countries);
+    } catch (error) {
+      console.error(error);
     }
-  }
-  console.log('===== async and await')
-  fetchData()
+  };
+  
+  console.log('===== async and await');
+  fetchData();
+  
