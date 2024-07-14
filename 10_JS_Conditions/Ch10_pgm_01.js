8d14c963f30f51c5db967fd04505cd9124cd01e1
32Nandhini <anand21nandhini@gmail.com>
2024-07-14 14:08:49 +0530

    Constructors

--- 10_JS_Conditions/Ch10_pgm_01.js
+++ 10_JS_Conditions/Ch10_pgm_01.js
@@ -9,6 +9,15 @@ var guess = function (userNumber) {
 };
 
 
+//futures
+var secret = 8;
+
+var guess = function (userNumber) {
+  if (userNumber > secret) {
+    console.log("Too high!");
+  }
+};
+
 
 /* Further Adventures
  *
