b78babe664f741e1827dd1e417435a3e843a5ec7
32Nandhini <anand21nandhini@gmail.com>
2024-07-14 14:06:37 +0530

    Constructors

--- 09_JS_Constructors/Ch9_pgm_05.js
+++ 09_JS_Constructors/Ch9_pgm_05.js
@@ -25,7 +25,47 @@ planet.addMoon("Europa");
 
 planet.showPlanet();
 
+// Define the Planet constructor with additional methods
+var Planet = function (name, position, type) {
+    this.name = name;
+    this.position = position;
+    this.type = type;
+    this.moons = [];
+  
+    this.showPlanet = function () {
+        var info = this.name + ": planet " + this.position;
+        info += " - " + this.type;
+        console.log(info);
+        console.log("Moons: " + (this.moons.length ? this.moons.join(', ') : 'None') + ".");
+    };
+  
+    this.addMoon = function (moon) {
+        this.moons.push(moon);
+    };
 
+    this.removeMoon = function () {
+        this.moons.pop();
+    };
+};
+
+// Create the first planet and add moons
+var planet1 = new Planet("Jupiter", 5, "Gas Giant");
+
+planet1.addMoon("Io");
+planet1.addMoon("Europa");
+
+planet1.showPlanet(); // Jupiter: planet 5 - Gas Giant. Moons: Io, Europa.
+
+
+var planet2 = new Planet("Saturn", 6, "Gas Giant");
+
+planet2.addMoon("Titan");
+planet2.addMoon("Enceladus");
+planet2.addMoon("Mimas");
+
+planet2.showPlanet(); 
+planet2.removeMoon();
+planet2.showPlanet(); 
 
 /* Further Adventures
  *
