eb355c1c5bdbe158f0007bdf550c50bc3514f4f2
32Nandhini <anand21nandhini@gmail.com>
2024-07-14 14:15:55 +0530

    Promises

--- 11_JS_Promises/Ch11_pgm_02.js
+++ 11_JS_Promises/Ch11_pgm_02.js
@@ -1,7 +1,7 @@
 // Callbacks
 
 // In this case the err is false and it will return the else block which is the result.
-
+/*
 const doSomething = callback => {
     setTimeout(() => {
       const skills = ['HTML', 'CSS', 'JS']
@@ -19,3 +19,19 @@ const doSomething = callback => {
 
 // after 2 seconds it will print the skills
 // ["HTML", "CSS", "JS"]
+*/
+
+//answers
+const doSomething = callback => {
+  setTimeout(() => {
+    const skills = ['HTML', 'CSS', 'JS'];
+    callback(false, skills);
+  }, 2000);
+};
+
+doSomething((err, result) => {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log(result);
+});
